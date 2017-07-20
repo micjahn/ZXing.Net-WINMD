e43102dc1c154cd1146ba24d68a8db9a5a0875ab
micjahn_cp <091957ed9bc2beaf9976b8124634cc4bfb41899c8UyuZhmm>
2017-07-20 15:31:17 +0000

    merged last changes from main branch
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS05/]$/ZXingNet/branches/WINMD;C90236

--- Source/lib/datamatrix/encoder/DefaultPlacement.cs
+++ Source/lib/datamatrix/encoder/DefaultPlacement.cs
@@ -21,7 +21,7 @@ namespace ZXing.Datamatrix.Encoder
    /// <summary>
    /// Symbol Character Placement Program. Adapted from Annex M.1 in ISO/IEC 16022:2000(E).
    /// </summary>
-   public sealed class DefaultPlacement
+   internal sealed class DefaultPlacement
    {
       private readonly String codewords;
       private readonly int numrows;
